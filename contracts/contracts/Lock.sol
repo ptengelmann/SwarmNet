47573a4aabcc426f484e58cf7f59689d2391e309
Pedro Serapiao <166184305+ptengelmann@users.noreply.github.com>
2025-03-29 19:32:49 +0000

    Initial folder structure with React, Express, and Hardhat

---
+++ contracts/contracts/Lock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SwarmNet {
+    // Just a placeholder for now
+    string public marketplaceName;
+
+    constructor() {
+        marketplaceName = "SwarmNet Marketplace";
+    }
+}
